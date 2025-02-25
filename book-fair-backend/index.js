fa67e95442feab387f430583763359ed91c63b55
Shivansh40 <117930564+Shivansh40@users.noreply.github.com>
2025-02-25 17:10:24 +0530

    made changes for vercel

--- book-fair-backend/index.js
+++ book-fair-backend/index.js
@@ -1,24 +1,24 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-//const userRoutes = require("./routes/userRoutes");
+
 const speakerRoutes = require("./routes/speakerRoutes");
 const eventRoutes = require("./routes/eventRoutes");
 const bannerRoutes = require("./routes/bannerRoutes");
-const publisherRoutes=require("./routes/publisherRoutes");
+const publisherRoutes = require("./routes/publisherRoutes");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-const PORT = process.env.PORT || 5000;
-
-//app.use("/users", userRoutes);
 app.use("/speakers", speakerRoutes);
 app.use("/events", eventRoutes);
 app.use("/banners", bannerRoutes);
 app.use("/publishers", publisherRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+
+app.get("/", (req, res) => {
+  res.send("Book Fair Backend is Running on Vercel!");
 });
+
+module.exports = app; 
